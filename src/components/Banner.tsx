5804f5d6ac9862506c08d6eb2086902706b2d02c
Leandro Barbosa <leandrobarbosa@sfr.fr>
2025-06-17 18:23:25 +0200

    - clean up banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@
 
 export default function Banner() {
   return (
-    <div className="flex items-center gap-x-6 bg-gray-900 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+    <div className="flex items-center justify-center gap-x-6 bg-gray-900 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
       <p className="text-sm/6 text-white">
         <a href="#">
           <strong className="font-semibold">Formwise</strong>
